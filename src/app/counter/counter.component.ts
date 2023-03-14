a8b76e401c5fe1159a7f8585d57393de45fd3289
MartyInFuture <martyinfuture@gmail.com>
2023-03-14 12:22:20 +0200

    add some basic components

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.sass']
+})
+export class CounterComponent {
+  count = 0;
+}
