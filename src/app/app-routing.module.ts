17ef316938b8691c5f401f500c4cf90c0e50c279
MartyInFuture <martyinfuture@gmail.com>
2023-03-17 13:57:19 +0200

    base directives overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 import { HomeComponent } from './home/home.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'counter', component: CounterComponent }
+  { path: 'counter', component: CounterComponent },
+  { path: 'directives', component: DirectivesComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
